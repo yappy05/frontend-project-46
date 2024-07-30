a3d933dd93afbbffdbc370efdf094ee9ca51de8e
sergey <wendingnn@gmail.com>
2024-07-30 12:18:56 +0300

    edit src/parseJSON and repait parseJSON test

--- __tests__/parseJSON.test.js
+++ __tests__/parseJSON.test.js
@@ -1,31 +1,26 @@
-import parseJSON from '../src/parseJSON.js';
+import parseJSON from "../src/parseJSON.js";
 
 let file;
 const fileJSON = {
-  host: 'hexlet.io',
+  host: "hexlet.io",
   timeout: 50,
-  proxy: '123.234.53.22',
+  proxy: "123.234.53.22",
   follow: false,
 };
 
-describe('correct path', () => {
-  test('parse file, absolutely path', () => {
-    file = '/Users/sergey/Documents/hexlet-projects/frontend-project-46/files/file1.json';
-    expect(parseJSON(file)).toEqual(fileJSON);
-  });
-
-  test('parse file, relative path', () => {
-    file = 'files/file1.json';
+describe("correct path", () => {
+  test("parse file, relative path", () => {
+    file = "files/file1.json";
     expect(parseJSON(file)).toEqual(fileJSON);
   });
 });
 
-describe('wrong path', () => {
-  test('no exist file', () => {
-    file = 'no/exist/file';
+describe("wrong path", () => {
+  test("no exist file", () => {
+    file = "no/exist/file";
     expect(parseJSON(file)).toBeNull();
   });
-  test('empety path', () => {
+  test("empety path", () => {
     expect(parseJSON()).toBeNull();
   });
 });
