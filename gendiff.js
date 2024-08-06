c530aa3b81365d67759e7db11003a4fe48f01bca
sergey <wendingnn@gmail.com>
2024-08-06 16:59:23 +0300

    create local npm package

---
+++ gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import getdiff from "./src/getdiff.js";
+
+program.description("Compares two configuration files and shows a difference.");
+program.version("0.0.1");
+program.option("-f, --format [type]", "output format", "stylish");
+program
+  .arguments("<filePath1>", "path to first file")
+  .arguments("<filePath2>", "path to first file");
+program.action((filePath1, filePath2) => {
+  console.log(getdiff(filePath1, filePath2));
+  // console.log(parseJSON(filePath1));
+  // console.log(parseJSON(filePath2));
+  //   options.format = 'json';
+});
+
+program.parse();
